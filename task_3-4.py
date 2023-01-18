2b6c37380eb0307a6ff9398f24ba82d1ade8cf59
Vitali Shnitko <stigmator565@gmail.com>
2023-01-18 21:03:57 +0300

    Finish file task_3-4.py

--- task_3-4.py
+++ task_3-4.py
@@ -3,37 +3,37 @@
 # и производит обратную операцию, получая исходный текст.
 
 
-# digits = set('0123456789')
-# i = 0
-# multiplier = ''
-# decrypted = ''
-#
-# with open('task222.txt') as in_f_obj:
-#     string = in_f_obj.readline().strip()
-#
-# char = string[i]
-# i += 1
-#
-# while i < len(string):
-#
-#     while string[i] in digits:
-#         multiplier += string[i]
-#         i += 1
-#         if i > (len(string) - 1):
-#             break
-#
-#     # print(char * int(multiplier), end='')
-#     decrypted += (char * int(multiplier))
-#
-#     multiplier = ''
-#     if i > (len(string) - 1):
-#         break
-#     char = string[i]
-#
-#     i += 1
-#
-# with open('answer22.txt', 'w') as out_f_obj:
-#     out_f_obj.write(decrypted)
+digits = set('0123456789')
+i = 0
+multiplier = ''
+decrypted = ''
+
+with open('task222.txt') as in_f_obj:
+    string = in_f_obj.readline().strip()
+
+char = string[i]
+i += 1
+
+while i < len(string):
+
+    while string[i] in digits:
+        multiplier += string[i]
+        i += 1
+        if i > (len(string) - 1):
+            break
+
+    # print(char * int(multiplier), end='')
+    decrypted += (char * int(multiplier))
+
+    multiplier = ''
+    if i > (len(string) - 1):
+        break
+    char = string[i]
+
+    i += 1
+
+with open('answer22.txt', 'w') as out_f_obj:
+    out_f_obj.write(decrypted)
 
 
 # Напишите программу, которая считывает текст из файла
@@ -42,28 +42,28 @@
 # Если таких слов несколько, вывести лексикографически первое
 # (можно использовать оператор < для строк).
 
-# dictionary = {}
-#
-# with open('dataset_3363_3.txt') as in_f_obj:
-#     for line in in_f_obj:
-#         line = line.lower()
-#         for word in line.split():
-#             if word not in dictionary:
-#                 dictionary[word] = 1
-#             elif word in dictionary:
-#                 dictionary[word] += 1
-#
-# max_quantity = 1
-#
-# for key, value in dictionary.items():
-#     current_quantity = dictionary[key]
-#     if current_quantity > max_quantity:
-#         max_quantity = current_quantity
-#         word_with_max_quantity = key
-#
-# with open('dataset_3363_3333.txt', 'w') as out_f_obj:
-#     most_popular = (word_with_max_quantity + ' ' + str(max_quantity))
-#     out_f_obj.write(most_popular)
+dictionary = {}
+
+with open('dataset_3363_3.txt') as in_f_obj:
+    for line in in_f_obj:
+        line = line.lower()
+        for word in line.split():
+            if word not in dictionary:
+                dictionary[word] = 1
+            elif word in dictionary:
+                dictionary[word] += 1
+
+max_quantity = 1
+
+for key, value in dictionary.items():
+    current_quantity = dictionary[key]
+    if current_quantity > max_quantity:
+        max_quantity = current_quantity
+        word_with_max_quantity = key
+
+with open('dataset_3363_3333.txt', 'w') as out_f_obj:
+    most_popular = (word_with_max_quantity + ' ' + str(max_quantity))
+    out_f_obj.write(most_popular)
 
 
 # Имеется файл с данными по успеваемости абитуриентов.

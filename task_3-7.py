83a0290d0abddb620d0b75eac09d5b6cd040e05e
Vitali Shnitko <stigmator565@gmail.com>
2023-01-19 19:58:37 +0300

    Added solution 1 task.

--- task_3-7.py
+++ task_3-7.py
@@ -15,3 +15,42 @@
 
 Порядок вывода команд произвольный.
 '''
+
+n = int(input())
+teams = {}
+for i in range(n):
+    team1, goal1, team2, goal2 = input().split(';')
+
+    if team1 not in teams:
+        teams[team1] = {'plays': 0, 'wins': 0, 'draws': 0, 'loses': 0, 'score': 0}
+
+    if team2 not in teams:
+        teams[team2] = {'plays': 0, 'wins': 0, 'draws': 0, 'loses': 0, 'score': 0}
+
+    if goal1 > goal2:  # team1 wins
+        teams[team1]['wins'] += 1
+        teams[team1]['score'] += 3
+
+        teams[team2]['loses'] += 1
+        teams[team2]['score'] += 0
+
+    elif goal2 > goal1:  # team2 wins
+        teams[team1]['loses'] += 1
+        teams[team1]['score'] += 0
+
+        teams[team2]['wins'] += 1
+        teams[team2]['score'] += 3
+
+    elif goal1 == goal2:  # draw
+        teams[team1]['draws'] += 1
+        teams[team1]['score'] += 1
+
+        teams[team2]['draws'] += 1
+        teams[team2]['score'] += 1
+
+    teams[team1]['plays'] += 1
+    teams[team2]['plays'] += 1
+
+for team in teams:
+    values_order = ['plays', 'wins', 'draws', 'loses', 'score']
+    print("{}:{}".format(str(team), ' '.join([str(teams[team][key]) for key in values_order])))
